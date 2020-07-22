cdb38d3957546f5d7d2959dddcec7aa4ed8f9884
Maurice K. | Yoka <48809800+YokaNivik@users.noreply.github.com>
2020-07-22 21:55:28 +0200

    Add files via upload
    removed Carte because it's not working yet

--- js/main.js
+++ js/main.js
@@ -7,24 +7,30 @@ window.onload = function () {
 
     //COLONNE DE DROITE
     const selection = document.getElementById("selection");
-
+ const selectedfood = document.querySelectorAll(".selectedfood")
     //const nourriture = document.getElementById("addItem").previousElementSibling.innerHTML;
     //const nourriture = document.getElementsByClassName("addItem")[0].previousElementSibling.innerHTML;
 
 
-
+    let i = 1;
 
     button.addEventListener("click", function () {
-        const nourriture = $(event.target.previousElementSibling).text();
-        console.log(nourriture)
-        //selection.innerHTML += "<p>" + nourriture + "</p>";
 
-        if (nourriture == selection) {
-            selection.innerHTML += "<p>" + "2x" + $(event.target.previousElementSibling).text() + "</p>";
-            return;
+       
+        console.log(selectedfood.length);
+
+        const nourriture = $(event.target.previousElementSibling).text();
+        //console.log(nourriture)
+        //console.log(selection.innerText);
+        //selection.innerHTML += "<p>" + nourriture + "</p>";
+        if (selection.innerHTML.indexOf(nourriture) !== -1) {
+            selection.innerHTML += "<p class=\"selectedfood \">" + i + "x" + $(event.target.previousElementSibling).text() + "</p>";
+            i++;
         } else
             // AFFICHER LE PRODUIT DANS LA COLONNE DE DROITE
             selection.innerHTML += "<p>" + $(event.target.previousElementSibling).text() + "</p>";
+        i++;
+
 
 
 
